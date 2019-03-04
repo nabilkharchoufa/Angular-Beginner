7693cc83f3618f8fb8b68f9b01ece8b06461142d
KHARCHOUFA NABIL <nabil.kharchoufa@open-groupe.com>
2019-03-04 17:49:33 +0100

    init commit step-01

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-  # Lab02
-  Le projet initial pour commencer les développement sur la session débutant Angular 
+Angular routing step-01
+=======================
 
-  Pour lancer l'applica
+- - - - 
+
+# Mettre en place le routage #
+
+    * Définir le base path (Optionnel)
+    * Import le router 
+    * Configurer la route 
+    * Mettre en place dans la template 
+    * Activate route
+    
+## définir le base path ##
+
+Deux façon pour le faire :
+
+   
+    1. Le faire à la main 
+    2. Utiliser Angular CLI
+
+`ng build --base-href /ateam/`
+
+`<base href="/">`
+
+`(localhost:4200)`
+
+`<base href="/ateam/">`
+
+## importer Angular Routing ##
+
+RouterModule c'est un module externe 
+
+Service =>  gérer la navigation et la manipulation de l'url  
+Configuration => pour configurer les routes 
+Directives => 
+- RouterLink=> les élements cliquable 
+- RouterLinkActive=> style des elements active 
+- RouterOutlet=> quand il faut afficher une template 
+
+Avant d'utiliser le routage il faut importer le RouterModule dans le app.module.ts
+import { RouterModule } from '@angular/router';
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule
+  ],...
+ 
--- src/app/films/edit-film/edit-film.component.ts
+++ src/app/films/edit-film/edit-film.component.ts
@@ -1,15 +1,84 @@
+import { FilmService } from './../film.service';
+import { Film } from './../film';
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/messages/message.service';
 
 @Component({
   selector: 'app-edit-film',
   templateUrl: './edit-film.component.html',
   styleUrls: ['./edit-film.component.css']
 })
-export class EditFilmComponent implements OnInit {
+export class EditFilmComponent {
+  pageTitle = 'Modification de film';
+  errorMessage: string;
 
-  constructor() { }
+  film: Film;
 
-  ngOnInit() {
+  constructor(private filmService: FilmService,
+              private messageService: MessageService) { }
+
+  getFilm(id: number): void {
+    this.filmService.getFilm(id)
+      .subscribe(
+        (film: Film) => this.onFilmRetrieved(film),
+        (error: any) => this.errorMessage = <any>error
+      );
   }
 
+  onFilmRetrieved(film: Film): void {
+    this.film = film;
+
+    if (!this.film) {
+      this.pageTitle = 'Aucun film';
+    } else {
+      if (this.film.id === 0) {
+        this.pageTitle = 'Ajouter un Film';
+      } else {
+        this.pageTitle = `Modifier le Film: ${this.film.filmName}`;
+      }
+    }
+  }
+
+  deleteFilm(): void {
+    if (this.film.id === 0) {
+      // Don't delete, it was never saved.
+      this.onSaveComplete(`${this.film.filmName} was deleted`);
+    } else {
+      if (confirm(`Confirmez vous la suppression le film : ${this.film.filmName}?`)) {
+        this.filmService.deleteFilm(this.film.id)
+          .subscribe(
+            () => this.onSaveComplete(`${this.film.filmName} a été supprimé, j'espère que vous savez ce que vous faites`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    }
+  }
+
+  saveFilm(): void {
+    if (true === true) {
+      if (this.film.id === 0) {
+        this.filmService.createFilm(this.film)
+          .subscribe(
+            () => this.onSaveComplete(`The new ${this.film.filmName} was saved`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      } else {
+        this.filmService.updateFilm(this.film)
+          .subscribe(
+            () => this.onSaveComplete(`The updated ${this.film.filmName} was saved`),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    } else {
+      this.errorMessage = 'Please correct the validation errors.';
+    }
+  }
+
+  onSaveComplete(message?: string): void {
+    if (message) {
+      this.messageService.addMessage(message);
+    }
+
+    // Navigate back to the film list
+  }
 }

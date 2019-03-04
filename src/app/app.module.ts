a5311615b1132cc5e952465645e24bd87e06e2d0
KHARCHOUFA NABIL <nabil.kharchoufa@open-groupe.com>
2019-03-04 17:49:33 +0100

    init commit step-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { FilmModule } from './films/film.module';
+import { MessageService } from './messages/message.service';
 import { FilmService } from './films/film.service';
 import { FilmData } from './films/films-data';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,26 +10,32 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ATeamComponent } from './a-team/a-team.component';
-import { FilmsComponent } from './films/films.component';
-import { EditFilmComponent } from './films/edit-film/edit-film.component';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ATeamComponent,
-    FilmsComponent,
-    EditFilmComponent
+    PageNotFoundComponent
   ],
   imports: [
     SharedModule,
+    FilmModule,
+    UserModule,
+    MessageModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(FilmData, { delay: 1000 }),
+    RouterModule.forRoot([
+      { path: 'welcome', component: ATeamComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ]),
     BrowserModule
   ],
-  providers: [FilmService],
+  providers: [FilmService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

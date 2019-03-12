5adaeb975eb34102ddb1e5ccb4b1651d89011ad9
KHARCHOUFA NABIL <nabil.kharchoufa@open-groupe.com>
2019-03-12 19:10:05 +0100

    clean readme and project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FilmModule } from './films/film.module';
 import { FilmService } from './films/film.service';
 import { FilmData } from './films/films-data';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,8 +6,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ATeamComponent } from './a-team/a-team.component';
-import { FilmsComponent } from './films/films.component';
-import { EditFilmComponent } from './films/edit-film/edit-film.component';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,14 +13,13 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    ATeamComponent,
-    FilmsComponent,
-    EditFilmComponent
+    ATeamComponent
   ],
   imports: [
     SharedModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(FilmData, { delay: 1000 }),
+    FilmModule,
     BrowserModule
   ],
   providers: [FilmService],
